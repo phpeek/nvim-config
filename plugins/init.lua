36d09b05ea3f355deb45cd0b1baad3e8c93012a6
Paweł Małolepszy <pawel.malolepszy@gmail.com>
2023-10-17 10:59:52 +0200

    feat: add fugitive

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,7 +1,13 @@
 local plugins = {
+  -- vim learning
   {
     "ThePrimeagen/vim-be-good", cmd = "VimBeGood"
-  }
+  },
+
+  -- git intergration
+  {
+    "tpope/vim-fugitive", cmd = "Git"
+  },
 }
 
 return plugins
