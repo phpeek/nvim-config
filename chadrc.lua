e0915908d01801501aedc635fb15bc85232ec2e8
Paweł Małolepszy <pawel.malolepszy@gmail.com>
2023-10-17 10:49:47 +0200

    feat: custom mappings

--- chadrc.lua
+++ chadrc.lua
@@ -1,7 +1,10 @@
 ---@type ChadrcConfig
-
 local M = {}
 
+-- load mappings
+M.mappings = require "custom.mappings"
+
+-- set theme
 M.ui = { theme = 'tokyonight' }
 
 return M
---
+++ mappings.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+M.general = {
+  i = {
+    ['kk'] = { "<esc>", "escape insert mode", opts = { nowait = true }},
+  }
+}
+
+return M
