09d40a5d5e4190e7267961bfac2d5c96e6fccaa4
Paweł Małolepszy <pawel.malolepszy@gmail.com>
2023-10-18 22:10:43 +0200

    feat: add custom options

--- init.lua
+++ init.lua
@@ -1,3 +1,22 @@
-local opt = vim.opt
+local opt = vim.o
 
+-- enable relative line numbers
 opt.relativenumber = true
+
+-- do not wrap long lines
+opt.wrap = false
+
+-- disable swap files 
+opt.swapfile = false
+
+-- disable backup file
+opt.backup = false
+
+-- keep 8 lines from top/bottom
+opt.scrolloff = 8
+
+-- disable searched highlights
+opt.hlsearch = false
+
+-- highlight incremental searc
+opt.incsearch = true
