15149de410ac8a6095c1a094e85c6cc6fb2132a4
Paweł Małolepszy <pawel.malolepszy@gmail.com>
2024-03-29 01:03:28 +0100

    opt -> o

--- lua/options.lua
+++ lua/options.lua
@@ -1,27 +1,27 @@
 require 'nvchad.options'
 
-local opt = vim.o
+local o = vim.o
 
 -- enable relative line numbers
-opt.relativenumber = true
+o.relativenumber = true
 
 -- do not wrap long lines
-opt.wrap = false
+o.wrap = false
 
 -- disable swap files
-opt.swapfile = false
+o.swapfile = false
 
 -- disable backup file
-opt.backup = false
+o.backup = false
 
 -- keep 8 lines from top/bottom
-opt.scrolloff = 8
+o.scrolloff = 8
 
 -- disable searched highlights
-opt.hlsearch = false
+o.hlsearch = false
 
 -- highlight incremental searc
-opt.incsearch = true
+o.incsearch = true
 
 -- highlight column at 80th character
-opt.colorcolumn = '80'
+o.colorcolumn = '80'
